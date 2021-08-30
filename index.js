783b7b0c0004452221fcae11dfc0076e28299f45
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-08-30 16:17:54 +0700

    update index.js

--- index.js
+++ index.js
@@ -11,25 +11,11 @@ fs.readFile(filePath, null, (err, data) => {
 
         let inData = new Uint8Array(data);
 
-        // lutis.gaussianBlur('.png', data, (err, res) => {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.log(data.slice(0, 10));
-        //         console.log(res.slice(0, 10));
-        //         fs.writeFile('out.png', res, 'binary', (err) => {
-        //             if (err) {
-        //                 console.log(err);
-        //             }
-        //         });
-        //     }
-            
-        // });
-
-        lutis.drawCircle('.png', 400, 400, 90, {R:245, G:245, B:73}, (err, res) => {
+        lutis.gaussianBlur('.png', data, (err, res) => {
             if (err) {
                 console.log(err);
             } else {
+                console.log(data.slice(0, 10));
                 console.log(res.slice(0, 10));
                 fs.writeFile('out.png', res, 'binary', (err) => {
                     if (err) {
@@ -40,7 +26,22 @@ fs.readFile(filePath, null, (err, data) => {
             
         });
 
-        // let res = lutis.inspect(data);
+        // lutis.drawCircle('.png', 400, 400, 90, {R:245, G:245, B:73}, (err, res) => {
+        //     if (err) {
+        //         console.log(err);
+        //     } else {
+        //         console.log(res.slice(0, 10));
+        //         fs.writeFile('out.png', res, 'binary', (err) => {
+        //             if (err) {
+        //                 console.log(err);
+        //             }
+        //         });
+        //     }
+            
+        // });
+
+        let res = lutis.inspect(data);
+        console.log(res);
 
         // fs.createWriteStream('out.png').write(res);
         
