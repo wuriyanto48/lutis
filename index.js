c0e219ae648748a8c0681442e727b03bde1285b3
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-08-30 09:41:30 +0700

    test operation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const lutis = require('./lib');
 
 const fs = require('fs');
 
-let filePath = 'elasticsearch_logo.png';
+let filePath = 'pp.jpg';
 
 fs.readFile(filePath, null, (err, data) => {
     if (err) {
@@ -15,13 +15,18 @@ fs.readFile(filePath, null, (err, data) => {
         console.log(data.slice(0, 10));
         console.log(res.slice(0, 10));
 
-        // fs.writeFile('out.jpg', res, 'binary', (err) => {
-        //     if (err) {
-        //         console.log(err);
-        //     }
-        // });
+        // let res = lutis.inspect(data);
 
-        fs.createWriteStream('out.png').write(res);
+        // let res = lutis.drawCircle('.png', 400, 400, 90);
+
+        fs.writeFile('out.png', res, 'binary', (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+
+        // fs.createWriteStream('out.png').write(res);
+        
     }
 });
 
