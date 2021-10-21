eb32f7f2bb04fb30a6b94c487cf04014dbeae66d
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-10-21 13:41:17 +0700

    test jpeg grayscale basic filter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const lutis = require('./lib');
 
 const fs = require('fs');
 
-let filePath = 'pp.webp';
+let filePath = 'pp.jpg';
 
 let res = lutis.generateJpeg('out.jpeg');
 fs.writeFile('out.jpeg', res, 'binary', (err) => {
@@ -15,46 +15,60 @@ fs.readFile(filePath, null, (err, data) => {
     if (err) {
         console.log(err);
     } else {
-
-        let inData = new Uint8Array(data);
-
-        // lutis.gaussianBlur('.png', data, (err, res) => {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.log(data.slice(0, 10));
-        //         console.log(res.slice(0, 10));
-        //         fs.writeFile('out.png', res, 'binary', (err) => {
-        //             if (err) {
-        //                 console.log(err);
-        //             }
-        //         });
-        //     }
-            
-        // });
-
-        // lutis.drawCircle('.png', 400, 400, 90, {R:245, G:245, B:73}, (err, res) => {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.log(res.slice(0, 10));
-        //         fs.writeFile('out.png', res, 'binary', (err) => {
-        //             if (err) {
-        //                 console.log(err);
-        //             }
-        //         });
-        //     }
-            
-        // });
-
-        // let res = lutis.resizeMagick(100, 800, 0.5, data);
+        let res = lutis.grayFilterJpeg(data);
         
-        // let res = lutis.drawTextMagick('hello world', data);
-        let res = lutis.decodeWebp(data);
-        console.log(res);
-
-        // fs.createWriteStream('out.png').write(res);
-        
+        fs.writeFile('gray_out.jpeg', res, 'binary', (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
     }
 });
+
+// fs.readFile(filePath, null, (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+
+//         let inData = new Uint8Array(data);
+
+//         // lutis.gaussianBlur('.png', data, (err, res) => {
+//         //     if (err) {
+//         //         console.log(err);
+//         //     } else {
+//         //         console.log(data.slice(0, 10));
+//         //         console.log(res.slice(0, 10));
+//         //         fs.writeFile('out.png', res, 'binary', (err) => {
+//         //             if (err) {
+//         //                 console.log(err);
+//         //             }
+//         //         });
+//         //     }
+            
+//         // });
+
+//         // lutis.drawCircle('.png', 400, 400, 90, {R:245, G:245, B:73}, (err, res) => {
+//         //     if (err) {
+//         //         console.log(err);
+//         //     } else {
+//         //         console.log(res.slice(0, 10));
+//         //         fs.writeFile('out.png', res, 'binary', (err) => {
+//         //             if (err) {
+//         //                 console.log(err);
+//         //             }
+//         //         });
+//         //     }
+            
+//         // });
+
+//         // let res = lutis.resizeMagick(100, 800, 0.5, data);
+        
+//         // let res = lutis.drawTextMagick('hello world', data);
+//         let res = lutis.decodeWebp(data);
+//         console.log(res);
+
+//         // fs.createWriteStream('out.png').write(res);
+        
+//     }
+// });
 
