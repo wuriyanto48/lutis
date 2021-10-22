b29895f466fd175d4d5442168c0bf9e6efb1bd3b
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-10-23 02:30:03 +0700

    test decode data

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const lutis = require('./lib');
 
 const fs = require('fs');
 
-let filePath = 'out.jpeg';
+let filePath = 'pp.webp';
 
-// let res = lutis.randomPixelJpeg('out.jpeg');
+// let res = lutis.randomPixelJpeg(5, 5);
 // fs.writeFile('out.jpeg', res, 'binary', (err) => {
 //     if (err) {
 //         console.log(err);
+//     }
+// });
+
+// fs.readFile(filePath, null, (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         let res = lutis.grayFilterJpeg(data);
+        
+//         fs.writeFile('gray_out.jpeg', res, 'binary', (err) => {
+//             if (err) {
+//                 console.log('------', err);
+//             }
+//         });
 //     }
 // });
 
@@ -15,60 +29,49 @@ fs.readFile(filePath, null, (err, data) => {
     if (err) {
         console.log(err);
     } else {
-        let res = lutis.grayFilterJpeg(data);
+
+        // lutis.gaussianBlur('.png', data, (err, res) => {
+        //     if (err) {
+        //         console.log(err);
+        //     } else {
+        //         console.log(data.slice(0, 10));
+        //         console.log(res.slice(0, 10));
+        //         fs.writeFile('out.png', res, 'binary', (err) => {
+        //             if (err) {
+        //                 console.log(err);
+        //             }
+        //         });
+        //     }
+            
+        // });
+
+        // lutis.drawCircle('.png', 400, 400, 90, {R:245, G:245, B:73}, (err, res) => {
+        //     if (err) {
+        //         console.log(err);
+        //     } else {
+        //         console.log(res.slice(0, 10));
+        //         fs.writeFile('out.png', res, 'binary', (err) => {
+        //             if (err) {
+        //                 console.log(err);
+        //             }
+        //         });
+        //     }
+            
+        // });
+
+        // let res = lutis.resizeMagick(100, 800, 0.5, data);
         
-        fs.writeFile('gray_out.jpeg', res, 'binary', (err) => {
+        // let res = lutis.drawTextMagick('hello world', data);
+        let res = lutis.decodeWebp(data);
+        console.log(res);
+        fs.writeFile('out.webp', res, 'binary', (err) => {
             if (err) {
-                console.log('------', err);
+                console.log(err);
             }
         });
+
+        // fs.createWriteStream('out.png').write(res);
+        
     }
 });
-
-// fs.readFile(filePath, null, (err, data) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-
-//         let inData = new Uint8Array(data);
-
-//         // lutis.gaussianBlur('.png', data, (err, res) => {
-//         //     if (err) {
-//         //         console.log(err);
-//         //     } else {
-//         //         console.log(data.slice(0, 10));
-//         //         console.log(res.slice(0, 10));
-//         //         fs.writeFile('out.png', res, 'binary', (err) => {
-//         //             if (err) {
-//         //                 console.log(err);
-//         //             }
-//         //         });
-//         //     }
-            
-//         // });
-
-//         // lutis.drawCircle('.png', 400, 400, 90, {R:245, G:245, B:73}, (err, res) => {
-//         //     if (err) {
-//         //         console.log(err);
-//         //     } else {
-//         //         console.log(res.slice(0, 10));
-//         //         fs.writeFile('out.png', res, 'binary', (err) => {
-//         //             if (err) {
-//         //                 console.log(err);
-//         //             }
-//         //         });
-//         //     }
-            
-//         // });
-
-//         // let res = lutis.resizeMagick(100, 800, 0.5, data);
-        
-//         // let res = lutis.drawTextMagick('hello world', data);
-//         let res = lutis.decodeWebp(data);
-//         console.log(res);
-
-//         // fs.createWriteStream('out.png').write(res);
-        
-//     }
-// });
 
