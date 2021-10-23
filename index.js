8513383d9bd7a3909ec0a86ed6b7fdd39b87a914
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-10-23 22:10:46 +0700

    ad CI

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const fs = require('fs');
 
 let filePath = 'pp.webp';
 
-// let res = lutis.randomPixelJpeg(5, 5);
-// fs.writeFile('out.jpeg', res, 'binary', (err) => {
-//     if (err) {
-//         console.log(err);
-//     }
-// });
+let res = lutis.randomPixelJpeg(5, 5);
+fs.writeFile('out.jpeg', res, 'binary', (err) => {
+    if (err) {
+        console.log('error... ', err);
+    } else {
+        console.log(res);
+    }
+});
 
 // fs.readFile(filePath, null, (err, data) => {
 //     if (err) {
@@ -25,53 +27,53 @@ let filePath = 'pp.webp';
 //     }
 // });
 
-fs.readFile(filePath, null, (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
+// fs.readFile(filePath, null, (err, data) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
 
-        // lutis.gaussianBlur('.png', data, (err, res) => {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.log(data.slice(0, 10));
-        //         console.log(res.slice(0, 10));
-        //         fs.writeFile('out.png', res, 'binary', (err) => {
-        //             if (err) {
-        //                 console.log(err);
-        //             }
-        //         });
-        //     }
+//         // lutis.gaussianBlur('.png', data, (err, res) => {
+//         //     if (err) {
+//         //         console.log(err);
+//         //     } else {
+//         //         console.log(data.slice(0, 10));
+//         //         console.log(res.slice(0, 10));
+//         //         fs.writeFile('out.png', res, 'binary', (err) => {
+//         //             if (err) {
+//         //                 console.log(err);
+//         //             }
+//         //         });
+//         //     }
             
-        // });
+//         // });
 
-        // lutis.drawCircle('.png', 400, 400, 90, {R:245, G:245, B:73}, (err, res) => {
-        //     if (err) {
-        //         console.log(err);
-        //     } else {
-        //         console.log(res.slice(0, 10));
-        //         fs.writeFile('out.png', res, 'binary', (err) => {
-        //             if (err) {
-        //                 console.log(err);
-        //             }
-        //         });
-        //     }
+//         // lutis.drawCircle('.png', 400, 400, 90, {R:245, G:245, B:73}, (err, res) => {
+//         //     if (err) {
+//         //         console.log(err);
+//         //     } else {
+//         //         console.log(res.slice(0, 10));
+//         //         fs.writeFile('out.png', res, 'binary', (err) => {
+//         //             if (err) {
+//         //                 console.log(err);
+//         //             }
+//         //         });
+//         //     }
             
-        // });
+//         // });
 
-        // let res = lutis.resizeMagick(100, 800, 0.5, data);
+//         // let res = lutis.resizeMagick(100, 800, 0.5, data);
         
-        // let res = lutis.drawTextMagick('hello world', data);
-        let res = lutis.decodeWebp(data);
-        console.log(res);
-        fs.writeFile('out.webp', res, 'binary', (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
+//         // let res = lutis.drawTextMagick('hello world', data);
+//         let res = lutis.decodeWebp(data);
+//         console.log(res);
+//         fs.writeFile('out.webp', res, 'binary', (err) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//         });
 
-        // fs.createWriteStream('out.png').write(res);
+//         // fs.createWriteStream('out.png').write(res);
         
-    }
-});
+//     }
+// });
 
